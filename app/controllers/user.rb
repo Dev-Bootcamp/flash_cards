b639ba4c8f82d8e8c5c285c82b72ba7b11874c51
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-20 18:28:04 -0500

    css tweaks

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -20,6 +20,7 @@ end
 
 get '/logout' do
   @user = nil
+  session.clear
   redirect to ('/')
 end
 
@@ -28,6 +29,7 @@ end
 post '/login' do
   @user = User.find_by(email: params[:user][:email])
   if @user && @user.password == params[:user][:password]
+    session[:user_id] = @user.id
     redirect to ("/user/#{@user.id}")
   else
     redirect to ('/invalid_login')
@@ -35,7 +37,7 @@ post '/login' do
 end
 
 post '/user/:id' do
-  puts "hi"
   @user = User.create(params[:user])
+  session[:user_id] = @user.id
   redirect to ("/user/#{@user.id}")
 end

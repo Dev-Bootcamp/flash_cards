ddddf6570c5b618bdb3263fe19b0e3acc65c081d
seanot <sean@kingfieldcapital.com>
2013-09-21 21:12:30 -0500

    Refactored the game controller

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -1,56 +1,29 @@
-
-
 get '/user/:id/deck/:deckid' do 
-  @user = User.find(params[:id])
-  @deck = Deck.find(params[:deckid])
+  @user =    User.find(params[:id])
+  @deck =    Deck.find(params[:deckid])
   @newcard = Deck.find(params[:deckid]).cards.first
-  @round = Round.create(user_id: params[:id], deck_id: params[:deckid])
+  @round =   Round.create(user_id: params[:id], deck_id: params[:deckid])
   erb :game
 end 
 
+#=======POST===================
+
 post '/user/:id/round/:roundid/deck/:deckid/card/:cardid' do
-  @user = User.find(params[:id])
-  @deck = Deck.find(params[:deckid])
+  @user =  User.find(params[:id])
+  @deck =  Deck.find(params[:deckid])
   @round = Round.find(params[:roundid])
-  puts @round 
-  @card = Card.find(params[:cardid])
-  newcardid = @card.id + 1
+  @card =  Card.find(params[:cardid])
+  new_card_id = @card.id + 1
   if @card.id < @deck.cards.last.id 
-    @newcard = Card.find(newcardid)
-    @guess  = Guess.create(card_id: params[:cardid], round_id: params[:roundid], guess: params[:answer])
-    if @guess.guess.downcase == @card.answer.downcase 
-      @result = "You are right"
-      newscore = @round.score += 1
-      @round.update_attributes(score: newscore) 
-      erb :game
-    else 
-      @result = "You are wrong"
-      erb :game
-    end 
+    @newcard = Card.find(new_card_id)
+    @guess = Guess.log_guess(@card, @round, params[:answer])
+    @result = Guess.check_answer(@guess, @card, @round)
+    erb :game
   else 
-    @guess  = Guess.create(card_id: params[:cardid], round_id: params[:roundid], guess:params[:answer])
-    if @guess.guess.downcase == @card.answer.downcase
-      newscore = @round.score += 1
-      @round.update_attributes(score: newscore)
-    end 
-
-    @percentage_score = (@round.score / @deck.cards.length.to_f) * 100
-
-    case @percentage_score
-    when 80..100
-      @comment = "You are Genius"
-    when 60..80
-      @comment = "You are pretty smart"
-    when 40..60
-      @comment = "You have got work to do"
-    when 20..40
-      @comment = "You are not very smart"
-    when 1..20
-      @comment = "You are bad at this"
-    else
-      @comment = "How did you get zero !? Where have you been"
-    end       
-
+    @guess = Guess.log_guess(@card, @round, params[:answer])
+    @result = Guess.check_answer(@guess, @card, @round)
+    @percentage_score = Guess.percentage(@round, @deck)
+    @comment = Guess.comment(@percentage_score)
     erb :summary
   end 
 end
--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,4 +1,43 @@
 class Guess < ActiveRecord::Base
   belongs_to :round
   belongs_to :card
+
+
+  def self.log_guess(card_id, round_id, answer)
+    Guess.create(card_id: card_id, round_id: round_id, guess: answer)
+  end
+
+
+  def self.percentage(round, deck)
+    (round.score / deck.cards.length.to_f) * 100
+  end
+
+
+  def self.check_answer(guess, card, round)
+    if guess.guess.downcase == card.answer.downcase 
+      newscore = round.score += 1
+      round.update_attributes(score: newscore) 
+      return "You are right"
+    else 
+      return "You are wrong"
+    end
+  end
+
+
+  def self.comment(percentage_score)
+    case percentage_score
+    when 80..100
+      return "You are a Genius"
+    when 60..80
+      return "You are pretty smart"
+    when 40..60
+      return "You have got work to do"
+    when 20..40
+      return "You are not very smart"
+    when 1..20
+      return "You are bad at this"
+    else
+      return "How did you get zero? Where have you been"
+    end       
+  end
 end
